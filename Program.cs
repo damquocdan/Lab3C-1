1c68a13dd653e1e6813786b1b172eaf60583ce85
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-07-28 15:20:26 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car c=new car();
+            c.make = "italy";
+            c.year = 2010;
+            c.model = "v1";
+            c.color = "red";
+
+
+            Console.WriteLine("Thông tin chi tiết");
+            Console.WriteLine("make:" + c.make);
+            Console.WriteLine("model:" + c.model);
+            Console.WriteLine("color" + c.color);
+            Console.WriteLine("year:" + c.year);
+            c.start();
+            c.stop();
+            Console.ReadKey ();
+        }
+    }
+}
---
+++ car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3C_
+{
+    internal class car
+    {
+        //khai báo các trường với phạm vi public
+        public string make;
+        public string model;
+        public string color;
+        public int year;
+        //Định nghĩa phương thức start
+        public void start ()
+        {
+            Console.WriteLine(model +"Khoi dong");
+        }
+        //Định nghĩa phương thức stop
+        public void stop()
+        {
+            Console.WriteLine(model+ "Khoi dong");
+        }
+    }
+}
